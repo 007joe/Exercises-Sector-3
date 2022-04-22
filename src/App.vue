3d026080318072b023a46297d4346854bcade51c
007joe <102744144+007joe@users.noreply.github.com>
2022-04-22 05:51:36 -0400

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="app">
-    <user-list></user-list>
+    <div id="nav">
+      <router-link to="/">Home</router-link>&nbsp;|&nbsp;
+      <router-link to="/add-topic">Add Topic</router-link>
+    </div>
+    <router-view />
   </div>
 </template>
 
-<script>
-import UserList from "./components/UserList.vue";
-
-export default {
-  name: "app",
-  components: {
-    UserList
-  }
-};
-</script>
-
-
- <style>
+<style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  max-width: 800px;
+  margin: 0 auto;
+}
+
+#nav {
+  padding: 30px;
+  text-align: center;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
